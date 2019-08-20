a5ba145fd7ee7d2ef085706d666015c3c0e5a007
Jane Slooth <sloothes.com@gmail.com>
2019-08-20 17:40:32 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+//  service-worker.js
+
+    self.version = 3.4;
+    var debugMode = true;
+
+    self.importScripts(
+        "/socketcluster.js",
+        "/sc-codec-min-bin.js",
+        "/js/Objectid.js",
+        "/js/zangodb.min.js",
+    );
+
+    var socket = socketCluster.create({
+        hostname: "anywhere3d.com",
+        codecEngine: scCodecMinBin,
+    });
+
+    var socket = socketCluster.create({
+        hostname: "anywhere3d.com",
+        codecEngine: scCodecMinBin,
+    });
+
+    socket.on("connect", function(status){
+        debugMode && console.log("[service-worker]:", {"status": status});
+    });
+
+    socket.on("error", function (err) {
+        console.error( "[service-worker]:", err );
+    });
+
+    socket.on("authStateChange", function( state ){
+        debugMode && console.log("[service-worker]:", {"authStateChange": state});
+    });
+
+    self.addEventListener("install", function(e){
+        debugMode && console.log("service worker installed.");
+
+        activate();
+
+    });
+
+    self.addEventListener("activate", function(e){
+        debugMode && console.log("service worker activated.");
+
+        clientsClaim();
+
+    });
+
+    function activate(){
+        debugMode && console.log("activating...");
+        self.skipWaiting();
+    }
+
+    function clientsClaim(){
+        debugMode && console.log("claiming...");
+        self.clients.claim();
+    }
